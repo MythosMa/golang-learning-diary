57c738c3ad34f69eb8bdad4da943ce96391d56b1
Ma Yuan <mythosmayuan@gmail.com>
2024-06-24 14:59:49 +0800

    add poke with go

--- pratice/poke/module/game.go
+++ pratice/poke/module/game.go
@@ -1,14 +1,13 @@
 package core
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
-	"fmt"
-
 )
 
 type Game struct {
-	Players []Player
+	Players     []Player
 	CardsInPool []Card
 }
 
@@ -16,52 +15,73 @@ func (game *Game) NewGame(players []string) {
 	cardColor := []string{"红桃", "黑桃", "梅花", "方片"}
 	cardValue := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
 	initCards := []Card{}
-	for i := 0 ; i < len(players) ; i++ {
-	    player := Player{Name: players[i], HandCards: []Card{}}
+	for i := 0; i < len(players); i++ {
+		player := Player{Name: players[i], HandCards: []Card{}}
 		game.Players = append(game.Players, player)
 	}
-	for i :=0 ; i < len(cardColor) ; i++ {
-		for j := 0 ; j < len(cardValue) ; j++ {
+	for i := 0; i < len(cardColor); i++ {
+		for j := 0; j < len(cardValue); j++ {
 			initCards = append(initCards, Card{Color: cardColor[i], Value: cardValue[j]})
 		}
 	}
 	initCards = shuffleCards(initCards)
 	// 发牌
 	playerIndex := 0
-	for i := 0 ; i < len(initCards) ; i++ {
-	    card := initCards[i]
+	for i := 0; i < len(initCards); i++ {
+		card := initCards[i]
 		game.Players[playerIndex].HandCards = append(game.Players[playerIndex].HandCards, card)
 		playerIndex++
 		if playerIndex == len(game.Players) {
-		    playerIndex = 0
+			playerIndex = 0
 		}
 	}
 	hasCardPlayerCount := len(game.Players)
 	for hasCardPlayerCount > 1 {
-		for i := 0 ; i < len(game.Players) ; i++ {
-			currentPlayer := game.Players[i];
-			roundPlayer(&currentPlayer, &game.CardsInPool)
+		for i := 0; i < len(game.Players); i++ {
+			// 在Go语言中，当你从切片中取出一个元素并赋值给一个新变量时，你得到的是该元素的拷贝，而不是原始元素的引用。所以想要修改切片中的值，需要使用指针。
+			currentPlayer := &game.Players[i]
+			if !currentPlayer.checkIsLost() {
+				game.CardsInPool = roundPlayer(currentPlayer, game.CardsInPool)
+				if currentPlayer.isNoCard() {
+					hasCardPlayerCount--
+					if hasCardPlayerCount <= 1 {
+						break
+					}
+				}
+			}
+		}
+	}
+	for i := 0; i < len(game.Players); i++ {
+		currentPlayer := game.Players[i]
+		if !currentPlayer.checkIsLost() {
+			currentPlayer.win()
 			return
+
 		}
 	}
 }
 
 func shuffleCards(cards []Card) []Card {
-	rand.Seed(time.Now().UnixNano())
+	seed := time.Now().UnixNano()
+	source := rand.NewSource(seed)
+	r := rand.New(source)
 	n := len(cards)
 	for i := n - 1; i > 0; i-- {
-		j := rand.Intn(i + 1)
+		j := r.Intn(i + 1)
 		cards[i], cards[j] = cards[j], cards[i]
 	}
 	return cards
 }
 
-func roundPlayer(player *Player, cardsInPool *[]Card) {
+func roundPlayer(player *Player, cardsInPool []Card) []Card {
 	fmt.Println("当前牌池:", cardsInPool)
 	playCard := player.PlayCard()
 	for index, card := range cardsInPool {
-	    if card.Color == playCard.Color && card.Value == playCard.Value {
-	        getCards := cardsInPool[index:]
-	    }
+		if card.Value == playCard.Value {
+			getCards := append(cardsInPool[index:], playCard)
+			player.getCards(getCards)
+			return cardsInPool[:index]
+		}
 	}
+	return append(cardsInPool, playCard)
 }
--- pratice/poke/module/player.go
+++ pratice/poke/module/player.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-
 )
 
 // Player 玩家
 type Player struct {
-    Name string
+	Name      string
 	HandCards []Card
-	isLost bool
+	isLost    bool
 }
 
 func (player *Player) PlayCard() Card {
+	// fmt.Printf("%s 还有 %d 张牌，分別是: %v\n", player.Name, len(player.HandCards), player.HandCards)
 	playCard, handCards := removeCard(player.HandCards)
 	player.HandCards = handCards
 	fmt.Printf("%s 出牌：%s%s\n", player.Name, playCard.Color, playCard.Value)
@@ -22,24 +22,38 @@ func (player *Player) PlayCard() Card {
 }
 
 func removeCard(handCards []Card) (Card, []Card) {
-	rand.Seed(time.Now().UnixNano())
-	index := rand.Intn(len(handCards))
-	return handCards[index], append(handCards[:index], handCards[index+1:]...)
+	seed := time.Now().UnixNano()
+	source := rand.NewSource(seed)
+	r := rand.New(source)
+
+	index := r.Intn(len(handCards))
+	removedCard := handCards[index]
+
+	newHandCards := make([]Card, len(handCards)-1)
+	copy(newHandCards, handCards[:index])
+	copy(newHandCards[index:], handCards[index+1:])
+	return removedCard, newHandCards
 }
 
 func (player *Player) isNoCard() bool {
-	if(len(player.HandCards) == 0) {
-		fmt.Printf("%s 没有牌了\n，退出了游戏", player.Name)
+	if len(player.HandCards) == 0 {
+		fmt.Printf("%s 没有牌了，退出了游戏\n", player.Name)
 		player.isLost = true
 		return true
 	}
+	// fmt.Printf("%s 还有 %d 张牌，分別是: %v\n", player.Name, len(player.HandCards), player.HandCards)
 	return false
 }
 
 func (player *Player) getCards(cards []Card) {
-    player.HandCards = append(player.HandCards, cards...)
+	fmt.Printf("%s 收走了 %d 张牌，分別是：%v\n", player.Name, len(cards), cards)
+	player.HandCards = append(player.HandCards, cards...)
 }
 
-func  (player *Player) checkIsLost() bool {
-    return player.isLost
+func (player *Player) checkIsLost() bool {
+	return player.isLost
+}
+
+func (player *Player) win() {
+	fmt.Printf("%s 获胜了\n", player.Name)
 }
